175569b7ee068ffd077eb47b9265bb5bd69cb888
KyleJ <kjacobson351@gmail.com>
2023-07-11 21:50:58 +0900

    remove git icon from index.html, remove code links, add slide feature to resume page

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 ///////SIDE BAR TOGGLE
+
+
+
 const sidebar = document.querySelector(".sidebar");
 
 const button = document.querySelector("#open-button");
@@ -13,6 +16,8 @@ closeButton.onclick = () => {
 sidebar.classList.toggle("open")
 }
 //////RESUME LAYOUT TOGGLE
+
+
 const resumeTogglebtn = document.querySelector("#toggle-resume-btn");
 
 const japaneseResume = document.querySelector(".japanese-resume");
@@ -20,3 +25,29 @@ const japaneseResume = document.querySelector(".japanese-resume");
 resumeTogglebtn.onclick = () => {
 japaneseResume.classList.toggle("layout");
 }
+
+//RESUME SLIDER
+
+
+const main1 = document.querySelector(".main1")
+const first1 = document.querySelector(".first-page-first-section")
+const first2 = document.querySelector(".first-page-second-section");
+const second1 = document.querySelector(".second-page-first-section");
+const main2 = document.querySelector(".main2");
+const slider = document.querySelector("#slider");
+const toggleResumeButton = document.querySelector("#toggle-resume-btn");
+slider.addEventListener("input", changeValue);
+function changeValue(){
+    const sliderValue = slider.value;
+    main2.style.transform = `translateX(${sliderValue}%)`;
+    if (sliderValue == 0) {
+        main1.style.display = "none"
+        toggleResumeButton.style.display = "none";   
+    } else {
+        main1.style.display = "grid";
+        toggleResumeButton.style.display = "block";
+        
+    }
+}
+
+
